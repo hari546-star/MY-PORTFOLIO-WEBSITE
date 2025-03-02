9e6143039e8f8f4b5a83f45d099f47652f24154b
hari546-star <harinaidu027@gmail.com>
2025-03-02 16:14:11 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('mouseover', () => {
+        card.style.transform = 'rotateY(180deg)';
+    });
+    card.addEventListener('
